4d57eb75ef53ed7457a3959d531a6edecc9ded74
Michelle Raouf <michelle.raouf@outlook.com>
2025-11-09 06:13:41 -0800

    Add initial implementation of IOSCrypto framework

--- Package.swift
+++ Package.swift
@@ -4,21 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "kmmcrypto",
+    name: "IOSCrypto",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "kmmcrypto",
-            targets: ["kmmcrypto"]),
+            name: "IOSCrypto",
+            targets: ["IOSCrypto"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "kmmcrypto"),
-        .testTarget(
-            name: "kmmcryptoTests",
-            dependencies: ["kmmcrypto"]
-        ),
+            name: "IOSCrypto"),
+    
     ]
 )
